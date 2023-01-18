f4bd252720a08a0048b234a6df76e48970790386
ksr-nivas <srinivask.ria@gmail.com>
2023-01-18 10:55:02 +0530

    3 files modified

--- src/app/input-form-example.ts
+++ src/app/input-form-example.ts
@@ -1,15 +1,71 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
 
 /**
  * @title Inputs in a form
  */
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
+export interface Task {
+  name: string;
+  completed: boolean;
+  color: ThemePalette;
+  subtasks?: Task[];
+}
+
 @Component({
   selector: 'input-form-example',
   templateUrl: 'input-form-example.html',
   styleUrls: ['input-form-example.css'],
 })
-export class InputFormExample {}
+export class InputFormExample {
+  foods: Food[] = [
+    { value: 'steak-0', viewValue: 'Steak' },
+    { value: 'pizza-1', viewValue: 'Pizza' },
+    { value: 'tacos-2', viewValue: 'Tacos' },
+  ];
 
+  task: Task = {
+    name: 'Hobbies',
+    completed: false,
+    color: 'primary',
+    subtasks: [
+      { name: 'Painting', completed: false, color: 'primary' },
+      { name: 'Chess', completed: false, color: 'accent' },
+      { name: 'Soccer', completed: false, color: 'warn' },
+    ],
+  };
+
+  allComplete: boolean = false;
+
+  updateAllComplete() {
+    this.allComplete =
+      this.task.subtasks != null &&
+      this.task.subtasks.every((t) => t.completed);
+  }
+
+  someComplete(): boolean {
+    if (this.task.subtasks == null) {
+      return false;
+    }
+    return (
+      this.task.subtasks.filter((t) => t.completed).length > 0 &&
+      !this.allComplete
+    );
+  }
+
+  setAll(completed: boolean) {
+    this.allComplete = completed;
+    if (this.task.subtasks == null) {
+      return;
+    }
+    this.task.subtasks.forEach((t) => (t.completed = completed));
+  }
+}
 
 /**  Copyright 2023 Google LLC. All Rights Reserved.
     Use of this source code is governed by an MIT-style license that
